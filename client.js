ff21262e386e6ae002d7022375894a8c85733fc8
Riley Webster <riley_ww77@yahoo.com>
2020-07-25 09:30:42 -0500

    got data appending

--- client.js
+++ client.js
@@ -4,9 +4,26 @@ $('document').ready(handleReady);
 
 let totalStaff = [];
 
+//let tableHeader = ['First Name', 'Last Name', 'ID Number', 'Job Title', 'Annual Salary'];
+    /*first: 'First Name',
+    second: 'Second Name',
+    third: 'ID Number',
+    fourth: 'Job Title',
+    fifth: 'Annual Salary'
+
+}*/
+
+
+//making table headers
+//$('table').append(`<tr><th>${tableHeader}</th></tr>`);
+//${tableHeader.first} ${tableHeader.second} ${tableHeader.third} ${tableHeader.fourth} ${tableHeader.fifth}`);
+//first} ${second} ${third} ${fourth} ${fifth
+
 function handleReady(){
     $('#submitButton').on('click', submitClicker);
 }
+
+
 
 function submitClicker(){
         console.log('button clicked');
@@ -42,13 +59,19 @@ function submitClicker(){
 
     function appendItemsToDom(){
         //clear dom of old info
-        $('#target').empty();
+        $('table').empty();
         //actually appending the items
+        
+        $('table').append(`<tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>ID Number</th>
+            <th>Job Title</th>
+            <th>Annual Salary</th>
+        </tr>`)
         for (let staff of totalStaff){
-            $('#target').append(`
-            <li>
-                ${staff.fName} ${staff.lName} ${staff.idNum} ${staff.jobTitle} ${staff.annualSalary}
-            </li>
-            `)
+            $('table').append(`
+            <tr><td>${staff.fName}</td> <td>${staff.lName} </td><td>${staff.idNum} </td><td>${staff.jobTitle} </td><td>${staff.annualSalary}</td></tr>
+            `);
         }
     }
